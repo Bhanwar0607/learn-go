e684602dbdfff60363fae139ad366768e71c0e4c
bhanwar0607 <bhavar1011@gmail.com>
2019-04-14 08:34:38 +0530

    Added the comments

--- hello-world/helloworld.go
+++ hello-world/helloworld.go
@@ -1,13 +1,17 @@
 package main
 
+// import the packages 
+// fmt: for print the text
+// net/http: to handle a request and response and start server.
 import (
 	"fmt"
 	"net/http"
 )
 
+// main(): main function is the entry point in golang program 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { // default route handling in golang 
+		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path) // print the text
 	})
-	http.ListenAndServe(":80", nil)
+	http.ListenAndServe(":80", nil) // start server
 }
